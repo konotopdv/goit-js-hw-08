a6800499833b155c1f4f6d77bbfdac6b28f3afe4
konotopdv <konotopdv@gmail.com>
2023-06-11 18:11:31 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -18,6 +18,7 @@ const getCurrentTime = function (currentTime) {
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
 player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+
 player
   .setColor('#d8e0ff')
   .then(function (color) {
