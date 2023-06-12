e600c5c6c6b53d883ec8cd60163053f483fe53af
konotopdv <konotopdv@gmail.com>
2023-06-12 15:30:46 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,62 +1,29 @@
 import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+var throttle = require('lodash.throttle');
 
-// --------------- 1st variant --------------------
+const playerEl = document.querySelector('iframe');
+const player = new Player('playerEl');
+const key = 'videoplayer-current-time';
+const currentTime = windows.localStorage.getItem(key);
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-
-// const onPlay = function (data) {
-//     localStorage.setItem('videoplayer-current-time', data.seconds);
-// };
-
-// player.on('timeupdate', throttle(onPlay, 1000));
-
-// const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
-
-// player.setCurrentTime(currentTime).then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function (error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-// player.setColor('#45a247').then(function (color) {
-//     // the color that was set
-// }).catch(function (error) {
-//     // an error occurred setting the color
-// });
-
-// --------------- 2nd variant --------------------
-const CURRENT_TIME_KEY = 'videoplayer-current-time';
-
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe, {
-  loop: true,
-  fullscreen: true,
-  quality: '1080p',
-});
-
-const getCurrentTime = function (currentTime) {
-  const seconds = currentTime.seconds;
-  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
-};
-
-player.on('timeupdate', throttle(getCurrentTime, 1000));
-
-player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
+player.on(
+  'timeupdate',
+  throttle(data => {
+    window.localStorage.setItem(key, data.seconds);
+  }, 1000)
+);
 
 player
-  .setColor('#d8e0ff')
-  .then(function (color) {
-    console.log('The new color value: #D8E0FF');
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    seconds = currentTime;
   })
   .catch(function (error) {
-    console.log('An error occurred while setting the color');
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
   });
